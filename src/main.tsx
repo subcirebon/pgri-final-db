e7d0168519dc2038f7d5a6456f8d9b213013eaad
subcirebon <subcirebon@gmail.com>
2026-01-29 22:07:43 +0700

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Import BrowserRouter dari react-router-dom
+import { BrowserRouter } from 'react-router-dom' 
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    {/* APP HARUS DIBUNGKUS BROWSER ROUTER AGAR MENU BISA DIKLIK */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
