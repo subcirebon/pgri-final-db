cc266071fc626160cb8e50baf873c5063359c317
subcirebon <subcirebon@gmail.com>
2026-02-04 23:31:41 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import Register from './Register';
 
 // --- IMPORT HALAMAN ---
 import Dashboard from './Dashboard'; 
-import AdminDatabase from './AdminDatabase'; // Dulu Members.tsx (Admin)
-import MyCard from './MyCard';               // Dulu Member.tsx (User)
+import AdminDatabase from './AdminDatabase'; // Dulu Members.tsx
+import MyCard from './MyCard';               // Dulu Member.tsx
 import Letters from './Letters';    
 import Finance from './Finance'; 
 import Donations from './Donations';
@@ -17,7 +17,6 @@ import Info from './Info';
 import Profile from './Profile';
 
 function App() {
-  // Cek status login dari LocalStorage
   const [isAuthenticated, setIsAuthenticated] = useState(() => {
     return localStorage.getItem('pgri_auth') === 'true';
   });
@@ -30,7 +29,6 @@ function App() {
     return localStorage.getItem('pgri_name') || 'Anggota';
   });
 
-  // Fungsi Login
   const handleLogin = (role: string, name: string, id: string) => {
     setIsAuthenticated(true);
     setUserRole(role);
@@ -42,7 +40,6 @@ function App() {
     localStorage.setItem('pgri_user_id', id.toString());
   };
 
-  // Fungsi Logout
   const handleLogout = () => {
     setIsAuthenticated(false);
     setUserRole('user');
@@ -52,7 +49,6 @@ function App() {
 
   return (
     <Routes>
-      {/* Route Public (Login & Register) */}
       <Route 
         path="/login" 
         element={!isAuthenticated ? <Login onLogin={handleLogin} /> : <Navigate to="/" replace />} 
@@ -63,19 +59,13 @@ function App() {
         element={!isAuthenticated ? <Register /> : <Navigate to="/" replace />} 
       />
 
-      {/* Route Protected (Butuh Login) */}
       <Route 
         path="/" 
         element={isAuthenticated ? <Layout onLogout={handleLogout} userRole={userRole} userName={userName} /> : <Navigate to="/login" replace />}
       >
         <Route index element={<Dashboard />} />
-        
-        {/* HALAMAN ADMIN: Database Anggota */}
         <Route path="members" element={<AdminDatabase />} />
-        
-        {/* HALAMAN USER: Kartu Saya */}
         <Route path="my-card" element={<MyCard />} /> 
-
         <Route path="letters" element={<Letters />} />
         <Route path="finance" element={<Finance />} />
         <Route path="donations" element={<Donations />} />
