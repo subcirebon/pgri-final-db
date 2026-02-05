fb8594319efdd60dfb50134a2321cd72e476bbce
subcirebon <subcirebon@gmail.com>
2026-02-05 09:18:09 +0700

    fix login route

--- src/App.tsx
+++ src/App.tsx
@@ -58,7 +58,7 @@ function App() {
       />
       
       <Route 
-        path="/register" 
+        path="/register/*" 
         element={!isAuthenticated ? <Register /> : <Navigate to="/" replace />} 
       />
 
